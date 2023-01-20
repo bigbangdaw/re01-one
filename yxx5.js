51eb3edca97bcb6fbbe759b11d9d0bee594ae759
bigbangdaw <119397341+bigbangdaw@users.noreply.github.com>
2023-01-20 14:20:03 +0800

    Add files via upload

---
+++ yxx5.js
@@ -0,0 +1,10 @@
+// const DEFAULT_COLORS = [210, 270, 255, 30, 285].map(item => `hsla(${item}, 100%, 50%, 1)`)
+
+const DEFAULT_COLORS = ["#FFFF00", '#F5D488', '#F5B488', '#DCBBA3', '#BE88DC',
+  '#FFE2A0', '#C9FFA0', '#A0ECFF', '#A0C0FF', '#FFA0A0']
+
+  const STATUS = {
+    INIT: 'init',
+    COMPLETED: 'completed'
+  
+  }
